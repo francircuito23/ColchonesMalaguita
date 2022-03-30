10ac21a70c384a79ffa18398713e915089b97ef4
francircuito23 <fgo0002@alu.medac.es>
2022-03-30 17:03:43 +0200

    logins

--- index.php
+++ index.php
@@ -90,13 +90,11 @@ if (!isset($_SESSION['access_token'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
 </head>
-
 <body>
     <div class="container">
         <br />
-        <h2 align="center" style="text-align: center;"> Inicio de sesión con <img src="img/logoGoogle.png"></h2>
+        <h2 style="text-align: center;"> Inicio de sesión con <img src="img/logoGoogle.png"></h2>
         <br />
         <div style="width: 130%; position: relative; margin-left: -160px">
             <div class="col-lg-4 offset-4">
