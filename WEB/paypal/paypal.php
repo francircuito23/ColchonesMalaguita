d38c093447bf2ab9e9131d9aad8b4fcd0458a433
francircuito23 <fgo0002@alu.medac.es>
2022-04-01 13:49:59 +0200

    web paypal

--- WEB/paypal/paypal.php
+++ WEB/paypal/paypal.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paypal</title>
+    <link rel="stylesheet" href="css/paypal.css">
     <script src="https://www.paypal.com/sdk/js?client-id=Ae26Wo0DGc-aTnLARCnJWud5tdWqHeRD4_rT5DthyDSeNh6ypkC5-hNB1LxCyJspBY8Q46GA4hU37lF9&currency=EUR"></script>
 </head>
 <body>
@@ -21,13 +22,34 @@
                 return actions.order.create({
                     purchase_units: [{
                         amount: {
-                            value: 999
+                            value: 1000000
                         }
                     }]
                 });
             },
+
+            onApprove: function(datos, actions){
+                actions.order.capture().then(function (detalles){
+
+                    
+                });
+            },
+
             onCancel: function(datos){
-                alert("Pago cancelado");
+                //alert("Pago cancelado");
+                var divCancel = document.createElement("div");
+                var divOpaco = document.createElement("div");
+
+                document.body.appendChild(divOpaco).appendChild(divCancel);
+
+                // divOpaco.className = "opaco";
+
+                document.body.style.backgroundColor = "rgba(117, 106, 106, 0.644)";
+
+                divCancel.textContent = "Pago cancelado";
+
+                divCancel.className = "cancelado";
+
             }
         }).render('#paypal-button-container');
     </script>
